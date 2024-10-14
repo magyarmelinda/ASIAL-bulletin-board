612328c03a548855755d5d19061175d9b75ba986
Magyar Melinda Barbara <m4gy4r.m3l1nd4@gmail.com>
2024-10-14 01:31:03 +0000

    chore: reformat, commenting

--- www/js/app.js
+++ www/js/app.js
@@ -1,62 +1,62 @@
-document.addEventListener('DOMContentLoaded', e => {
-  const applicationKey = '**********';
-  const clientKey = '**********';
+document.addEventListener("DOMContentLoaded", (e) => {
+  const applicationKey = "**********"; // Configure the application key here
+  const clientKey = "**********"; // Configure the client key here
   window.ncmb = new NCMB(applicationKey, clientKey);
-})
+});
 
 ons.ready(async () => {
-  // get the currently logged in user data
+  // Get the data of the currently logged-in user
   let user = ncmb.User.getCurrentUser();
-  // if there is a user = logging in is performed
+  // If a user exists, the login process is performed
   if (user) {
     try {
-      // it will try to get the data of the appropriate data store
-      // checks if the session is valid
-      await ncmb.DataStore('Test').fetch();
+      // It will attempt to retrieve data from the appropriate data store
+      // Checks if the session is valid
+      await ncmb.DataStore("Test").fetch();
     } catch (e) {
-      // in case of error: session expired, user data has been deleted, etc.
-      // user data will be deleted and an anonymous authentication is performed
+      // In case of an error, such as session expiration or deletion of user data
+      // User data will be deleted, and anonymous authentication will be performed
       user = null;
     }
   }
-  // if there is no user data, perform anonymous authentication
+  // If there is no user data, perform anonymous authentication
   if (!user) {
     user = await ncmb.User.loginAsAnonymous();
   }
 
-  // after authetication:
-  // get the admin group
-  const role = await ncmb.Role.equalTo('roleName', 'admin').fetch();
-  // get the users group
+  // After authentication:
+  // Retrieve the admin group
+  const role = await ncmb.Role.equalTo("roleName", "admin").fetch();
+  // Retrieve the users group
   const users = await role.fetchUser();
-  // if you have your objectID, it means your an admin
-  window.admin = users.map(u => u.objectId).indexOf(user.objectId) > -1;
+  // If you have your object ID, it means you are an admin.
+  window.admin = users.map((u) => u.objectId).indexOf(user.objectId) > -1;
 });
-
 
 function loadImage(name, className) {
   if (!this.querySelector(className)) return;
-  ncmb.File.download(name, 'blob')
-    .then(blob => {
-      const fileReader = new FileReader();
-      fileReader.onload = () => {
-        this.querySelector(className).src = fileReader.result;
-      }
-      fileReader.readAsDataURL(blob) ;
-    })
+  ncmb.File.download(name, "blob").then((blob) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      this.querySelector(className).src = fileReader.result;
+    };
+    fileReader.readAsDataURL(blob);
+  });
 }
 
-// function to determine if it can be deleted
+// Function to determine if the item can be deleted
 function deletable(obj) {
   let bol = false;
-  // admins can always delete
+  // Admins can always delete
   if (window.admin) {
     bol = true;
   } else {
-    // if you are not an admin, check if you have delete permission
+    // If you are not an admin, check if you have delete permissions
     const user = ncmb.User.getCurrentUser();
     bol = user && obj.acl[user.objectId] && obj.acl[user.objectId].write;
   }
-  // show trash can icon if deleteable
-  return bol ? `<ons-icon data-object-id="${obj.objectId}" class="delete" icon="fa-trash"></ons-icon>` : '';
+  // Show the trash icon if the item is deletable
+  return bol
+    ? `<ons-icon data-object-id="${obj.objectId}" class="delete" icon="fa-trash"></ons-icon>`
+    : "";
 }
